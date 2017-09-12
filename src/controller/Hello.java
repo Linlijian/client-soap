6c40c1099c33b51fd08099aeaae73a9f8e130c9e
Linlijian <Linlijian@172.16.63.52>
2017-09-12 14:17:09 +0700

    first client

---
+++ src/controller/Hello.java
@@ -0,0 +1,70 @@
+package controller;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.URI;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+
+/**
+ * Servlet implementation class Hello
+ */
+@WebServlet("/Hello")
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+    	response.setContentType("text/html;charset-UTF-8");
+    	PrintWriter out = new PrintWriter(new OutputStreamWriter(response.getOutputStream(), "UTF8"), true);
+    	
+    	// value from web
+    	String name = request.getParameter("name");
+    	
+		// request and response RESTful
+		ClientConfig config = new DefaultClientConfig(); 
+		Client client = Client.create(config); 
+		WebResource service = client.resource(getBaseURI()); 
+		
+		// request get
+		ClientResponse clientResponse = service.path("rest").path("hello").path(name) .accept(MediaType.TEXT_HTML).get(ClientResponse.class);
+		
+		if (clientResponse.getStatus() != 200) {
+			   throw new RuntimeException("Failed : HTTP error code : "
+				+ clientResponse.getStatus());
+		}
+
+		
+		out.print(clientResponse);
+		
+		
+	}
+    
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+	
+	private static URI getBaseURI() { 
+		return UriBuilder.fromUri( "http://localhost:8080/RESTfulDemo/").build(); 
+	}
+
+
+}
