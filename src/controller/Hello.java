8222f032366b6d083aebf271700f85bf7217baf7
MSI\Poon <poon1412@gmail.com>
2017-09-13 18:56:13 +0700

    done only interface

--- src/controller/Hello.java
+++ src/controller/Hello.java
@@ -3,6 +3,7 @@ package controller;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.StringReader;
 import java.net.URI;
 
 import javax.servlet.ServletException;
@@ -12,6 +13,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriBuilder;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
@@ -41,16 +50,40 @@ public class Hello extends HttpServlet {
 		
 		// request get
 
-		ClientResponse clientResponse = service.path("player").path("findplayer").path(name) .accept(MediaType.TEXT_HTML).get(ClientResponse.class);
+		ClientResponse clientResponse = service.path("player").path("findplayer").path(name) .accept(MediaType.TEXT_XML).get(ClientResponse.class);
 
 		if (clientResponse.getStatus() != 200) {
 			   throw new RuntimeException("Failed : HTTP error code /: "
 				+ clientResponse.getStatus());
 		}
 
+		String outputFromXml =  clientResponse.getEntity(String.class);
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();  
+	    DocumentBuilder builder;  
+	    Document doc = null;
+	    try  
+	    {  
+	        builder = factory.newDocumentBuilder();  
+	        doc = builder.parse( new InputSource( new StringReader( outputFromXml ) ) );
+	        doc.getDocumentElement().normalize();
+	    } catch (Exception e) {  
+	        e.printStackTrace();  
+	    } 
+	
+		NodeList nList  = doc.getElementsByTagName("player");
 		
-		out.print(clientResponse);
-		
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+			Node nNode = nList.item(temp);
+			System.out.println("\nCurrent Element :" + nNode.getNodeName());
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElement = (Element) nNode;
+				
+				out.println(eElement.getElementsByTagName("name").item(0).getTextContent());
+				out.println(eElement.getElementsByTagName("lname").item(0).getTextContent());
+	
+			}
+		}
+	
 		// output web
 		response.setContentType("text/html;charset-UTF-8");
 		out.print("<html>");
@@ -71,7 +104,7 @@ public class Hello extends HttpServlet {
 	}
 	
 	private static URI getBaseURI() { 
-		return UriBuilder.fromUri( "http://localhost:8080/tradefootball-server/fb").build(); 
+		return UriBuilder.fromUri( "http://localhost:8080/tradefootball-server/find").build(); 
 	}
 
 
